d528b284ef9d7d110406002d88d3b76682201a7b
userA <geniusssbg@gmail.com>
2023-04-20 00:28:01 +0800

    Initial commit

---
+++ console/src/main/java/org/example/ConsoleApplication.java
@@ -0,0 +1,19 @@
+
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Genius
+ * @date 2023/04/20 00:16
+ **/
+
+@SpringBootApplication
+public class ConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleApplication.class, args);
+
+    }
+}
