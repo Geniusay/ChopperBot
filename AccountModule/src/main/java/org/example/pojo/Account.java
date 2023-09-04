daa618d57387dc8c0f2b09e563874d4c5f97ae68
welsir <1824379011@qq.com>
2023-09-04 22:15:20 +0800

    账号模块-切片推送通道插件#20

---
+++ AccountModule/src/main/java/org/example/pojo/Account.java
@@ -0,0 +1,18 @@
+package org.example.pojo;
+
+/**
+ * @Description
+ * @Author welsir
+ * @Date 2023/9/4 22:08
+ */
+public class Account {
+    private String password;
+    private String videoType;
+
+    public Account() {
+    }
+
+    public String toString() {
+        return "Account{password='" + this.password + "', videoType='" + this.videoType + "'}";
+    }
+}
---
+++ AccountModule/src/main/java/org/example/pojo/VideoQueue.java
@@ -0,0 +1,56 @@
+package org.example.pojo;
+
+import org.example.core.VideoType;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author welsir
+ * @Date 2023/9/4 22:09
+ */
+public class VideoQueue {
+    private String name;
+    private List<Object> messages;
+    private List<VideoType> type;
+    private boolean isStrongMatch;
+
+    public List<VideoType> getType() {
+        return this.type;
+    }
+
+    public void setType(List<VideoType> type) {
+        this.type = type;
+    }
+
+    public VideoQueue() {
+    }
+
+    public VideoQueue(String name, boolean isStrongMatch) {
+        this.name = name;
+        this.messages = new ArrayList();
+        this.isStrongMatch = isStrongMatch;
+    }
+
+    public void enqueue(Object message) {
+        this.messages.add(message);
+    }
+
+    public Object dequeue() {
+        return !this.isEmpty() ? this.messages.remove(0) : null;
+    }
+
+    public boolean isEmpty() {
+        return this.messages.isEmpty();
+    }
+
+    public boolean shouldReceiveMessage(List<VideoType> videoType) {
+        return (new HashSet(videoType)).equals(new HashSet(this.type));
+    }
+
+    public String toString() {
+        return "[name:" + this.name + ",message:{" + this.messages + "},isMatch:" + this.isStrongMatch;
+    }
+}
